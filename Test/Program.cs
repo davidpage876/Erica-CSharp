031ff102e981032388bef3ee785eb4b0d7515647
David C. Page <davidpage876@gmail.com>
2024-04-23 20:31:50 +0930

    Create countdown program

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,11 +1,35 @@
-﻿namespace MyProject;
+﻿
+namespace MyProject;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        string name = "Erica";
+        var bomb = new Bomb();
+        await bomb.TriggerCountdown(5);
+    }
+}
 
-        Console.WriteLine(string.Format("Hello, {0}!", name));
+class Bomb
+{
+    public async Task TriggerCountdown(int countDownFrom)
+    {
+        var timer = new PeriodicTimer(TimeSpan.FromSeconds(1.0));
+        var secondsRemaining = countDownFrom;
+
+        Console.WriteLine("This bomb will explode in...");
+        while (await timer.WaitForNextTickAsync()) 
+        {
+            if (secondsRemaining > 0)
+            {
+                Console.WriteLine(string.Format("{0} seconds,", secondsRemaining));
+                secondsRemaining--;
+            }
+            else
+            {
+                Console.WriteLine("BOOM!");
+                timer.Dispose();
+            }
+        }
     }
 }
