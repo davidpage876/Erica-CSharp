780ff9b1d0c50b30c9ebd1572d132b41017dc8fb
evild <evild@DAVE-PC>
2024-04-23 18:58:59 +0930

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Erica";
+
+        Console.WriteLine(string.Format("Hello, {0}!", name));
+    }
+}
